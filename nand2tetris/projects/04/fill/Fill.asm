0c59bc5e3731a557696f9625fc2fb11c011df5ec
Michalis Papadopoullos <mechatr0nic.cy@gmail.com>
2019-10-09 23:19:53 +0300

    completed fill.asm

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -11,4 +11,72 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(LOAD_BASE_ADDR_1)
+@SCREEN // A = SCREEN = 16834
+D=A     // D => HOLDS THE BASE ADDRESS OF SCREEN MEMORY-MAP
+@ADDR   // TEMPORARY VARIABLE => WILL BE USED AS POINTER
+M=D     // RAM[ADDR] = 16384
+
+@8191   // *** IT MIGHT BE OFF BY ONE ***
+D=A     // LOAD #ROWS IN D
+@COUNTER
+M=D     // INITIALIZE COUNTER TO #ROWS
+
+// ---------------------------------
+
+(LOOP_WHITE)
+@ADDR   // A = ADDR
+A=M     // A = RAM[ADDR]
+M=0     // SET PIXELS TO WHITE
+
+@ADDR
+M=M+1   // ADVANCE POINTER TO THE NEXT ROW
+
+@COUNTER
+MD=M-1   // DECREMENT COUNTER
+@LOOP_WHITE
+D;JGT    // IF IS GREATER THAN ZERO, REPEAT
+
+// ---------------------------------
+
+// CHECK IF ANY KEY IS PRESSED
+(CHECK)
+@KBD
+D=M
+@LOAD_BASE_ADDR_1
+D;JEQ
+
+// ---------------------------------
+
+(LOAD_BASE_ADDR_2)
+@SCREEN // A = SCREEN = 16834
+D=A     // D => HOLDS THE BASE ADDRESS OF SCREEN MEMORY-MAP
+@ADDR   // TEMPORARY VARIABLE => WILL BE USED AS POINTER
+M=D     // RAM[ADDR] = 16384
+
+@8191   // *** IT MIGHT BE OFF BY ONE ***
+D=A     // LOAD #ROWS IN D
+@COUNTER
+M=D     // INITIALIZE COUNTER TO #ROWS
+
+// ---------------------------------
+
+// WRITE 1 (=> BLACK) TO EACH PIXEL
+(LOOP_BLACK)
+@ADDR   // A = ADDR
+A=M     // A = RAM[ADDR]
+M=-1
+
+@ADDR
+M=M+1   // ADVANCE POINTER TO THE NEXT ROW
+
+@COUNTER
+MD=M-1   // DECREMENT COUNTER
+@LOOP_BLACK
+D;JGT    // IF IS GREATER THAN ZERO, REPEAT
+
+// ---------------------------------
+
+// INFINITE LOOP
+@CHECK
+0;JMP
