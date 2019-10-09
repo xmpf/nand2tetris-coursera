e724772acd949f821672ec421bc9d6f6dcea2959
Michalis Papadopoullos <mechatr0nic.cy@gmail.com>
2019-10-09 18:37:16 +0300

    completed mult.asm w/ useful comments

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,57 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+(INIT)
+@R2
+M=0
+
+(CHECK)
+// CHECK IF EITHER RAM[1] == 0
+// AND GOTO OUTPUT
+@R1
+D=M
+@OUTPUT
+D;JEQ
+
+// CHECK IF EITHER RAM[0] == 0
+// AND GOTO OUTPUT
+@R0
+D=M     // D = RAM[0]
+@OUTPUT
+D;JEQ
+
+// USE A TEMPORARY VARIABLE
+// AS A COUNTER
+@R1
+D=M     // D = RAM[1]
+@temp
+M=D     // RAM[temp] = D <= RAM[1]
+
+(LOOP)
+// LOAD NUMBER STORED IN RAM[0]
+@R0
+D=M
+// AND ADD IT TO RAM[2]
+// WHICH IS USED AS AN ACCUMULATOR
+@R2
+M=M+D
+
+// LOAD COUNTER FROM RAM[temp]
+// DECREMENT IT
+// GOTO LOOP IF > 0
+@temp
+MD=M-1
+@LOOP   
+D;JGT    // IF D > 0 => repeat
+
+(OUTPUT)
+// LOAD ACCUMULATOR RAM[2]
+// AND STORE IT IN D FOR OUTPUT
+@R2
+D=M
+
+(END)
+// END OF PROGRAM
+// INFINITE LOOP
+@END
+0;JMP
